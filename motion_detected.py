c2a0017d126be3d829e2ecd4d92bed3e81019c5d
Caroline Dunn <thecarolinedunn@gmail.com>
2020-04-29 17:31:51 -0400

    Delete motion_detected.py

--- motion_detected.py
+++
@@ -1,7 +0,0 @@
-from Tkinter import *
-import tkMessageBox
-import vlc
-
-media = vlc.MediaPlayer("/home/pi/Pimoroni/speakerphat/test/test.mp3")
-media.play()
-tkMessageBox.showinfo("Motion Detected!", "Hello World")
