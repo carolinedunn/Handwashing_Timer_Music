b600e7f4ad3e86e25ea938d51302f2e328a34003
Caroline Dunn <thecarolinedunn@gmail.com>
2020-04-29 18:00:59 -0400

    Add files via upload

--- test_music.py
+++ test_music.py
@@ -6,7 +6,7 @@ import random
 
 
 x = random.randint(1,10)
-song = '/home/pi/Music/' + str(x) + '.mp3'
+song = '/home/pi/Handwashing_Timer_Music/music/' + str(x) + '.mp3'
 media = vlc.MediaPlayer(song)
 #media = vlc.MediaPlayer("/home/pi/Pimoroni/speakerphat/test/test.mp3")
 media.play()
